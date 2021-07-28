a745832299c7c59b9a1387b103fb2a828af43f57
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2021-07-29 00:04:57 +0200

    docs: Update README.MD

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ios-architecture-sample
-iOS Architecture Design Pattern Sample Code
+<div align="center">
+    <a href="https://sunggweon.dev/TIL/">
+      <h3>👷‍♂️</h3>
+        <h1>iOS Architecture Sample</h1>
+    </a>
+  iOS Architecture Design Pattern Sample Code
+</div>
+
+## 📚 Projects
+
+| Title | Date | Link |
+|-------|------|------|
+| Simple Viper + API Network | 2021.07  | [Link](https://github.com/iosdevted/ios-architecture-sample/tree/master/VIPER) |
+| VIPER + TDD + CoreData | 2021.07.28 ~  | [Link](https://github.com/iosdevted/ios-architecture-sample/tree/master/VIPER%2BTDD%2BCOREDATA) |
+
+
+
+
---
+++ VIPER+TDD+COREDATA/VIPER+TDD+COREDATATests/CoreData/CoreDataServiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoTests.swift
+//  VIPER+TDD+COREDATATests
+//
+//  Created by Ted on 2021/07/28.
+//
+
+import XCTest
+@testable import VIPER_TDD_COREDATA
+
+class CoreDataServiceTests: XCTestCase {
+    
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
