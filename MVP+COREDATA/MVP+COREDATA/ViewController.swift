c385cc121ce9571ac69f87694f910d86156bc823
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2021-08-02 20:59:49 +0200

    Initial commit

---
+++ MVP+COREDATA/MVP+COREDATA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP+COREDATA
+//
+//  Created by Ted on 2021/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
