45b972c4aab2ea9f7d95dece8184178a86da2a7e
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2021-09-09 22:12:36 +0900

    feat: Add Box for boxing and binding

--- MVVM+API/MVVM+API/Utility/Box.swift
+++ MVVM+API/MVVM+API/Utility/Box.swift
@@ -7,3 +7,22 @@
 
 import Foundation
 
+class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
