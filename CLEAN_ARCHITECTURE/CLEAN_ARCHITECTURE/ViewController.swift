ea07366cfc35eb8385432ed8c0b1b31d484bb585
iosdevted <devted@protonmail.com>
2021-10-17 17:35:24 +0200

    Initial commit

---
+++ CLEAN_ARCHITECTURE/CLEAN_ARCHITECTURE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLEAN_ARCHITECTURE
+//
+//  Created by Sunggweon Hyeong on 17/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
